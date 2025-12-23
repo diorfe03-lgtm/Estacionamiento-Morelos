50f9ad2b3f422719d58699aa8c9abf1021b159c7
Diego Ortega Fernandez <a01028535@tec.mx>
2025-12-22 21:44:35 -0600

    cambio *

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post("/corte-caja", async (req, res) => {
 
 // 🌐 SERVIR FRONTEND
 app.use(express.static(path.join(__dirname, "public")));
-app.get("*", (req, res) => {
+app.get("(.*)", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
